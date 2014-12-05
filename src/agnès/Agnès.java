6a8d6358327da90272de10cf85d8281e87bdc903
asauva <agnes.sauvaget@sfr.fr>
2014-12-05 09:30:33 +0100

    Initialisation du projet lapin

---
+++ src/agnès/Agnès.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agnès;
+
+/**
+ *
+ * @author Agnès
+ */
+public class Agnès {
+static int i = 0;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+       Rabbit rabbit = new Rabbit("bob", 5);
+       while(i <= 10){
+            rabbit.talk();
+       i=i+1;
+       }
+    }
+    
+}
---
+++ src/agnès/Rabbit.java
@@ -0,0 +1,42 @@
+package agnès;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Agnès
+ */
+public class Rabbit {
+    
+    private String name;
+    private int age;
+
+    public Rabbit(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+public void talk(){
+    System.out.println("HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+}
+
+}
